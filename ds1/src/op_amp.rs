9801d66d2d2107025d5b274804e6c2ff4b45b53c
Dave Mollen <dave.mollen@infi.nl>
2024-05-27 16:47:07 +0200

    Update op-amp

--- ds1/src/op_amp.rs
+++ ds1/src/op_amp.rs
@@ -6,6 +6,11 @@ pub struct OpAmp {
 }
 
 impl OpAmp {
+  const R1: f32 = 100000.;
+  const R2: f32 = 4700.;
+  const C1: f32 = 2.5e-10;
+  const C2: f32 = 1e-6;
+
   pub fn new(sample_rate: f32) -> Self {
     let double_sr = sample_rate * 2.;
 
@@ -20,14 +25,12 @@ impl OpAmp {
   pub fn process(&mut self, input: f32, dist: f32) -> f32 {
     let dist = dist.max(0.00001);
 
-    let rt = dist * 100000.;
-    let rb = ((1. - dist) * 100000.) + 4700.;
-    let cz = 0.000001;
-    let cc = 0.00000000025;
+    let rt = dist * Self::R1;
+    let rb = ((1. - dist) * Self::R1) + Self::R2;
 
-    let a = (rt * rb * cz * cc).recip();
-    let c = (rt * cc).recip() + (rb * cz).recip();
-    let b = c + (rb * cc).recip();
+    let a = (rt * rb * Self::C2 * Self::C1).recip();
+    let c = (rt * Self::C1).recip() + (rb * Self::C2).recip();
+    let b = c + (rb * Self::C1).recip();
 
     let a0 = a + c * self.double_sr + self.squared_double_sr;
     let b0 = (a + b * self.double_sr + self.squared_double_sr) / a0;
--- sketches/transfer-functions/op_amp.py
+++ sketches/transfer-functions/op_amp.py
@@ -18,8 +18,8 @@ def get_z_domain_coefficients(dist):
 
   rt = dist * 100000.
   rb = ((1. - dist) * 100000.) + 4700.
-  cz = 0.000001
-  cc = 0.00000000025
+  cz = 1e-6
+  cc = 2.5e-10
 
   a = 1. / (rt * rb * cz * cc)
   c = 1. / (rt * cc) + 1. / (rb * cz)
