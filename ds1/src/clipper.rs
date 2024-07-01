58376712ba6b11810c346fdd48e2f0c6e065c641
Dave Mollen <dave.mollen@infi.nl>
2024-07-01 16:19:41 +0200

    Use diode limiter simulation

--- ds1/src/clipper.rs
+++ ds1/src/clipper.rs
@@ -1,30 +1,54 @@
-mod oversample;
-use crate::shared::one_pole_filter::OnePoleFilter;
-use oversample::Oversample;
-use std::simd::{f32x8, StdFloat};
+const R: f32 = 2200.;
+const C: f32 = 1e-8;
+const IS: f32 = 2.52e-9;
+const VT: f32 = 0.0453;
 
 pub struct Clipper {
-  lowpass_filter: OnePoleFilter,
-  oversample: Oversample,
+  fs: f32,
+  x: f32,
+  yn1: f32,
 }
 
 impl Clipper {
   pub fn new(sample_rate: f32) -> Self {
     Self {
-      lowpass_filter: OnePoleFilter::new(sample_rate, 7230.),
-      oversample: Oversample::new(),
+      fs: sample_rate,
+      x: 0.,
+      yn1: 0.,
     }
   }
 
   pub fn process(&mut self, input: f32) -> f32 {
-    let filtered = self.lowpass_filter.process(input);
+    let mut yc: f32 = self.yn1; // Current value of the output (being solved for)
+    let mut res: f32 = 10.; // set residual above limit of while loop
 
-    self.oversample.process(filtered, |x| {
-      let x2 = x * x;
-      let x3 = x2 * x;
-      let x5 = x3 * x2;
-      let a = x + f32x8::splat(0.16489087) * x3 + f32x8::splat(0.00985468) * x5;
-      a / (f32x8::splat(1.0) + a * a).sqrt()
-    }) * 0.305496 // 0.610992 * 0.5
+    for _ in 0..100 {
+      if res < 1e-12 {
+        break;
+      }
+
+      // Calculate the function value
+      let f = (input - yc) / R - ((2. * C * self.fs) * yc) + self.x - 2. * IS * (yc / VT).sinh();
+
+      // Calculate it's derivative
+      let j = -1. / R - (2. * C * self.fs) - (2. * IS / VT) * (yc / VT).cosh();
+
+      // Newton step is defined by the function divided by the derivative.
+      let step = -f / j;
+
+      // Only magnitude is usefuly for defining a residual
+      res = step.abs();
+
+      // Take a Newton step towards the solution (potentially)
+      yc = yc + step;
+    }
+
+    // Update last solution
+    self.yn1 = yc;
+
+    // Calculate state
+    self.x = (4. * C * self.fs) * yc - self.x;
+
+    yc * 0.5
   }
 }
